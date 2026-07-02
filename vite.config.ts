c1add3a4e71f22fb7468ccce86b72247692b2d68
uipowerranger-sys <uipowerranger@gmail.com>
2026-07-02 12:27:50 +0530

    feat: setup react structure and initial cicd workflow

---
+++ vite.config.ts
@@ -0,0 +1,13 @@
+// Import defineConfig from 'vitest/config' instead of 'vite'
+import { defineConfig } from 'vitest/config'
+// import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [],
+  base: process.env.NODE_ENV === 'production' ? '/firstapp/' : '/',
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/test/setup.ts',
+  },
+})
