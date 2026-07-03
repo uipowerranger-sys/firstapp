1059efeba7101d984d4141e00d313823c946be5a
uipowerranger-sys <uipowerranger@gmail.com>
2026-07-03 18:13:53 +0530

    updating the vite config file

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,10 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/react-cicd-demo/' : '/',
+  // Change '/react-cicd-demo/' to '/firstapp/' to match your repository name
+  base: process.env.NODE_ENV === 'production' ? '/firstapp/' : '/',
   test: {
     globals: true,
     environment: 'jsdom',
-    // We removed the setupFiles line from here!
   },
 })
